5d6be86545a68bcf81a388734e67f900fb6a2232
Ian Wanjala Lukuya <66877@STD.STRATHMORE.LOCAL>
2022-10-26 20:04:02 +0300

    v1 python fiels

---
+++ 1_output.py
@@ -0,0 +1,35 @@
+# Datatypes in python
+
+#primitive datatypes
+
+x = 5 # int data type
+y = 2.5 # float data type
+
+
+#Python allows variable declarations on the same line
+# x , y = 5, 2.5
+
+#Python allows setting multiple variables to the same value
+a = b = c = 3
+
+my_string = ("Hello There") #Str data type
+is_true = True # Boolean data Type
+is_false = False # Boolean data type
+my_string = [1,2,3] #List data Type
+non_type = None
+
+my_char = ('a')
+my_string = ('Hello There') #Str data type
+is_true = True # Boolean data Type
+is_false = False # Boolean data type
+my_first_list = [1,2,3] #List data Type
+non_type = None
+
+print (type(x))
+print (type(y))
+print (type(a))
+print (type(my_string))
+print (type(is_true))
+print (type(my_first_list))
+print (type(non_type))
+print (type(my_char))
---
+++ 2_output.py
@@ -0,0 +1,17 @@
+# Datatypes in python 
+
+#primitive datatypes  
+
+x = 5 
+y = 2.5
+
+
+#Python allows variable declarations on the same line 
+# x , y = 5, 2.5 
+
+#Python allows setting multiple variables to the same value
+a = b = c = 3
+
+my_string = ("Hello There") #Str data type
+is_true = True # Boolean data Type
+is_true = False # Boolean data type
